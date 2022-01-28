dacd58bf48932c7e30e320f2dfe59cbfe2572980
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-01-28 22:20:19 +0100

    add a post method

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my api"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are your posts"}
+
+
+@app.post("/createpost")
+def get_posts():
+    return {"message": "Successfully created a post."}
