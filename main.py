d0de908bcd17bc0f51fbbc61ba4589dc90ad9393
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-01-28 23:18:25 +0100

    add my_post

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
+my_post = [{"title": "title 1", "content": "content 1", "id": 1},
+           {"title": "title 2", "content": "content 2", "id": 2}]
+
+
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -19,7 +26,8 @@ def get_posts():
     return {"data": "These are your posts"}
 
 
-@app.post("/createpost")
-def get_posts(new_post: Post):
-    print(new_post)
-    return {"data": "new_post"}
+@app.post("/posts")
+def get_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
