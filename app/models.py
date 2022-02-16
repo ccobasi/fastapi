a00260fdc65f60db0433b6dc59d586352ef98d67
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-02-16 11:54:01 +0100

    modified post table

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from tokenize import String
 from turtle import title
 from xmlrpc.client import Boolean
 from markupsafe import string
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, true
+from sqlalchemy.sql.expression import text
 from .database import Base
 
 
@@ -13,3 +14,5 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
