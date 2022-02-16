abaf2aadb5dbef354ed239740194ad875e7fcf2d
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-02-16 13:36:17 +0100

    modified update post

--- app/main.py
+++ app/main.py
@@ -115,15 +115,20 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """ UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s
-        returning *""",
-        (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+# def update_post(id: int, post:Post):
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """ UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s
+    #     returning *""",
+    #     (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} not found")
-
-    return {"data": updated_post}
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    # return {"data": updated_post}
+    return {"data": post_query.first()}
