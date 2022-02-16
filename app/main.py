43cc194c78932bf49a813eda447fd8bf1fa6a1c2
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-02-16 12:11:31 +0100

    add query

--- app/main.py
+++ app/main.py
@@ -53,7 +53,9 @@ async def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status": "success"}
+
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 @app.get("/posts")
